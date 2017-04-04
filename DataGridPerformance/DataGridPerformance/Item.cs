f74fd92851108023aab8cbb7d2749a6ec33ca4a6
Novikov Egor <e.novikov@intecom-energo.ru>
2017-04-04 17:11:54 +0300

    DataGridPerformance application added

---
+++ DataGridPerformance/DataGridPerformance/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace DataGridPerformance
+{
+    public class Item
+    {
+        public int Property1 { get; set; }
+        public int Property2 { get; set; }
+        public int Property3 { get; set; }
+        public int Property4 { get; set; }
+        public int Property5 { get; set; }
+        public int Property6 { get; set; }
+        public int Property7 { get; set; }
+        public int Property8 { get; set; }
+    }
+}
---
+++ DataGridPerformance/DataGridPerformance/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace DataGridPerformance
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Random _random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FillTable(object sender, RoutedEventArgs e)
+        {
+            MyDataGrid.ItemsSource = Enumerable.Range(0, 1000).Select(CreateItem).ToList();
+        }
+
+        private Item CreateItem(int arg)
+        {
+            return new Item
+            {
+                Property1 = _random.Next(-10000, 10000),
+                Property2 = _random.Next(-10000, 10000),
+                Property3 = _random.Next(-10000, 10000),
+                Property4 = _random.Next(-10000, 10000),
+                Property5 = _random.Next(-10000, 10000),
+                Property6 = _random.Next(-10000, 10000),
+                Property7 = _random.Next(-10000, 10000),
+                Property8 = _random.Next(-10000, 10000),
+            };
+        }
+    }
+}
